9bdca011cc95262a652255fb0521eb6f0e9f935f
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-12 15:31:13 -0800

    working on import

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+var mongoose = require('mongoose');
 var express = require('express');
+var app = express();
+var busRoutes = require(__dirname + '/routes/router');
+
+mongoose.connect('mongodb://localhost/'); //TODO insert path to db
+
+app.use('/api', busRoutes);
+
+app.listen(3000, function() {
+  console.log('server listening on port 3000');
+});
