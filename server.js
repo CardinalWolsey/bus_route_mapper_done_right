b7464379954f2719c985053e63e9a53a07d8a86e
Michael Tse <michael.tse3@gmail.com>
2015-11-13 10:55:23 -0800

    just saved database to mongo

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
-var playerRouter = require(__dirname + '/routes/player_routes');
 
-// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/mlb_player')
-
-// app.use('/api', playerRouter);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server running');
