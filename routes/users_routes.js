40579a449366f9b1e7d657361430d3dd6d54e148
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-11-13 14:13:45 -0800

    added users routes code outline

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -1,4 +1,41 @@
 var express = require('express');
-var bodyParser = require('body-parser')
-//fill in path
-var User = require(__dirname + )
+var bodyParser = require('body-parser');
+var User = require(__dirname + '/../models/user');
+var handleError = require(__dirname + '/../lib/handleServerError');
+
+var usersRouter = module.exports = exports = express.Router();
+
+usersRouter.get('/users', function(req, res) {
+  User.find({}, function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+usersRouter.post('/users', bodyParser.json(), function(req, res) {
+  var newUser = new User(req.body);
+  newUser.save(function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+usersRouter.put('/users/:id', bodyParser.json(), function(req, res) {
+  var userData = req.body;
+  delete userData._id;
+  User.update({_id: req.params.id}, userData, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'successfully updated a user!'});
+  });
+});
+
+usersRouter.delete('/users/:id', function(req, res) {
+  User.remove({_id: req.params.id}, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'You successfully deleted a user!'});
+  });
+});
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
-var busRoutes = require(__dirname + '/routes/router');
+var busRoutes = require(__dirname + '/routes/busdata_routes');
+var usersRoutes = require(__dirname + 'routes/users_routes');
 
-mongoose.connect('mongodb://localhost/busData'); 
+mongoose.connect('mongodb://localhost/busData');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users');
 
 app.use('/api', busRoutes);
+app.use('/api', usersRoutes)
 
 app.listen(3000, function() {
   console.log('server listening on port 3000');
