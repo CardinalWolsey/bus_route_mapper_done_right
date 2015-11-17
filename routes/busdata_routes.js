17ec35222f03a124d8cf919fc192478032c6557c
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-17 11:11:02 -0800

    basic nearbusroutes spatial query is done

--- routes/busdata_routes.js
+++ routes/busdata_routes.js
@@ -18,6 +18,7 @@ busRouter.post('/busroutes', bodyParser.json(), function(req, res) {
 });
 
 busRouter.get('/busroutes/:route_num', function(req, res) {
+  console.log('get single route request recieved');
   busRoute.find({"properties.RTE_NUM":req.params.route_num}, function(err, data) {
     if (err) return handleError(err, res);
 
@@ -42,3 +43,19 @@ busRouter.delete('/busroutes/:id', function(req, res) {
     res.json({msg: 'successfully deleted route with delete method'});
   });
 });
+
+busRouter.get('/nearbusroutes/', function(req, res) {
+  console.log('near request received ');
+  var lng = Number(req.query.lng);
+  var lat = Number(req.query.lat);
+  var radius = Number(req.query.radius);
+  console.log("Long, lat, radius : " + lng + ", " + lat + ", " + radius);
+  busRoute.find({geometry: { $near: {$geometry: {type: "Point", coordinates: [lng, lat]}, $maxDistance: radius}}}, function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+
+// db.routes.find( { geometry:{ $near: { $geometry: { type: "Point", coordinates: [-122.31, 47.6244] }, $maxDistance: 1000 } } },{geometry: 0} ).limit(2).pretty()
