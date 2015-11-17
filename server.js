7586fb29d6130d0de40d028966051a72d58dfb81
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-11-16 21:11:14 -0800

    attempt to fix potential merge conflicts take one

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
-var busRouter = require(__dirname + '/routes/busdata_routes');
-var authRouter = require(__dirname + '/routes/users_routes');
+var busRoutes = require(__dirname + '/routes/busdata_routes');
+var authRoutes = require(__dirname + '/routes/users_routes');
 
 mongoose.connect('mongodb://localhost/busData');
 // mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users');
 process.env.APP_SECRET = process.env.APP_SECRET || 'changemechangemechangeme';
 
-app.use('/api', busRouter);
-app.use('/api', authRouter)
+app.use('/api', busRoutes);
+app.use('/api', authRoutes)
 
 app.listen(3000, function() {
   console.log('server listening on port 3000');
