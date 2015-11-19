9ed96ec3a9138f8779def8d3d5fc4969a3f959ff
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-19 13:24:30 -0800

    edited server.js to add reference to PORT

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static('public'));
 app.use('/api', busRoutes);
 app.use('/api', authRoutes);
 
-app.listen(3000, function() {
-  console.log('server listening on port 3000');
+app.listen(process.env.PORT || 5000, function() {
+  console.log('server listening');
 });
