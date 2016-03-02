d2d63efdbc3958c7f69c296e16ab92ce1555221a
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-03-02 10:56:34 -0800

    added ng-cloak ... the regular directive had trouble with SASS or webpack, so I manually coded in a CSS rule ... also makde all colors not so close to white, so they do not fade on the map

--- app/js/map/controllers/map_controller.js
+++ app/js/map/controllers/map_controller.js
@@ -25,12 +25,12 @@ module.exports = function(app) {
 
     $scope.busRouteDatas = [];
 
-    var colorLibrary = [{inUse: false, colorFamily: ["#08519c", "#3182bd", "#6baed6", "#bdd7e7", "#eff3ff"]},
-                        {inUse: false, colorFamily: ["#006d2c", "#31a354", "#74c476", "#bae4b3", "#edf8e9"]},
-                        {inUse: false, colorFamily: ["#54278f", "#756bb1", "#9e9ac8", "#cbc9e2", "#f2f0f7"]},
-                        {inUse: false, colorFamily: ["#a50f15", "#de2d26", "#fb6a4a", "#fcae91", "#fee5d9"]},
+    var colorLibrary = [{inUse: false, colorFamily: ["#08306b", "#08519c", "#2171b5", "#4292c6", "#6baed6"]},
+                        {inUse: false, colorFamily: ["#00441b", "#006d2c", "#238b45", "#41ab5d", "#74c476"]},
+                        {inUse: false, colorFamily: ["#3f007d", "#54278f", "#6a51a3", "#807dba", "#9e9ac8"]},
+                        {inUse: false, colorFamily: ["#67000d", "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a"]},
                         {inUse: false, colorFamily: ["#99540F", "#B26F2C", "#CC8E51", "#E5B17E", "#FFD8B2"]},
-                        {inUse: false, colorFamily: ["#252525", "#636363", "#969696", "#cccccc", "#f7f7f7"]}];
+                        {inUse: false, colorFamily: ["#000000", "#252525", "#525252", "#737373", "#969696"]}];
 
     var findColorFamily = function() {
       for (var i = 0; i < colorLibrary.length; i++) {
